2f86a31bb1a512c06522fc901c338e84fb239f33
Justin <jgaston2606@gmail.com>
2024-12-04 14:15:11 -0500

    part 1 solution

--- Day1/Day01.kt
+++ Day1/Day01.kt
@@ -1,31 +1,37 @@
 package Day01.Day1
 import java.io.File
 import java.io.InputStream
+import kotlin.math.abs
 
 fun main(){
-    val inputFile = File("C:\\Users\\BT_4N2_02\\IdeaProjects\\Advent\\src\\Day1\\Day01_Input");
+    val inputFile = File("C:\\Users\\Justin\\IdeaProjects\\JustinAdvent2024\\Day1\\Day01_Input");
     val inputFileStream: InputStream =  inputFile.inputStream();
     val fileData = mutableListOf<String>();
     val leftNums = mutableListOf<String>()
+    val leftNumsInt = mutableListOf<Int>()
+    val rightNumsInt = mutableListOf<Int>()
     val rightNums = mutableListOf<String>()
     inputFileStream.bufferedReader().forEachLine {
         fileData.add(it);
     }
-    for (i in 0..fileData.size-1){
-       leftNums.add(fileData[i].substringBefore(" "))
+    for (i in 0..<fileData.size){
+        leftNums.add(fileData[i].substringBefore(" "))
     }
-    for (i in 0..fileData.size-1){
+    for (i in 0..<fileData.size){
         rightNums.add(fileData[i].substringAfter("   "))
     }
-    /*
-    for( i in 0..fileData.size-1){
-        leftNums[i].toInt()
-        rightNums[i].toInt()
+    for( i in 0..<fileData.size){
+        leftNumsInt.add(leftNums[i].toInt())
+        rightNumsInt.add(rightNums[i].toInt())
+
     }
-
-     */
-    leftNums.map { it.toInt()}
-    rightNums.map { it.toInt()}
-
-    println(leftNums[1]+5)
+    val sortedLeft = leftNumsInt.sorted()
+    val sortedRight = rightNumsInt.sorted()
+    var finalCount = 0;
+    for(i in sortedLeft.indices){
+        finalCount += abs(sortedLeft[i] - sortedRight[i])
+    }
+    println(sortedRight)
+    println(sortedLeft)
+    println(finalCount)
 }
