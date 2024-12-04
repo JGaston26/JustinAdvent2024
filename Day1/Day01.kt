a316361fedb9f2c2a8d33ca49cf1e2f87c0bba4a
JGaston26 <125668861+JGaston26@users.noreply.github.com>
2024-12-04 14:40:47 -0500

    Part 2 Solution Day01.kt

--- Day1/Day01.kt
+++ Day1/Day01.kt
@@ -1,16 +1,16 @@
 package Day01.Day1
 import java.io.File
 import java.io.InputStream
-import kotlin.math.abs
 
 fun main(){
-    val inputFile = File("C:\\Users\\Justin\\IdeaProjects\\JustinAdvent2024\\Day1\\Day01_Input");
+    val inputFile = File("Day1/Day01_Input");
     val inputFileStream: InputStream =  inputFile.inputStream();
     val fileData = mutableListOf<String>();
     val leftNums = mutableListOf<String>()
     val leftNumsInt = mutableListOf<Int>()
     val rightNumsInt = mutableListOf<Int>()
     val rightNums = mutableListOf<String>()
+
     inputFileStream.bufferedReader().forEachLine {
         fileData.add(it);
     }
@@ -28,11 +28,28 @@ fun main(){
     val sortedLeft = leftNumsInt.sorted()
     val sortedRight = rightNumsInt.sorted()
     var finalCount = 0;
+    /* Solution to Part One
     for(i in sortedLeft.indices){
         finalCount += abs(sortedLeft[i] - sortedRight[i])
     }
-
     println(sortedRight)
     println(sortedLeft)
     println(finalCount)
+
+     */
+
+    //Part 2 Solution
+    for(i in sortedLeft.indices){
+        finalCount += frequencyCounter(sortedLeft[i],sortedRight) * sortedLeft[i]
+    }
+    println(finalCount)
+}
+fun frequencyCounter(num: Int, arr: List<Int>) : Int{
+    var count = 0;
+    for(i in arr.indices){
+        if (arr[i] == num) {
+            count++
+        }
+    }
+    return count
 }
