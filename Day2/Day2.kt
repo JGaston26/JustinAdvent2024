7258d125d788cc08e2dc8a8fa2e3bf47a46b5c12
JGaston26 <125668861+JGaston26@users.noreply.github.com>
2024-12-05 14:49:58 -0500

    Update Day2.kt

--- Day2/Day2.kt
+++ Day2/Day2.kt
@@ -1,2 +1,25 @@
 package Day2
+import java.io.File
+import java.io.InputStream
+
+fun main(){
+    val inputFile = File("Day2/Day2_input")
+    val inputFileStream : InputStream = inputFile.inputStream()
+    val fileData = mutableListOf<String>()
+     inputFileStream.bufferedReader().forEachLine {
+         fileData.add(it)
+     }
+    println(fileData)
+    val noSpaces = mutableListOf<String>()
+
+}
+
+fun doFloorMath(myList : MutableList<String>) : Boolean{
+    var j =2
+    for(j in myList.indices){
+        j+=2
+        val floorString =
+    }
+}
+
 
