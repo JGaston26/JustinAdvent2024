53fbb9782126f3b24d82e6e5fed55a2cffa0daa6
JGaston26 <125668861+JGaston26@users.noreply.github.com>
2024-12-10 14:50:00 -0500

    Add files via upload

---
+++ Day3.kt
@@ -0,0 +1,23 @@
+import java.io.File
+import java.io.InputStream
+
+fun main(){
+    val inputFile = File("src/Day3_Input")
+    val inputFileStream : InputStream = inputFile.inputStream()
+    val fileData = mutableListOf<String>()
+    inputFileStream.bufferedReader().forEachLine {
+        fileData.add(it)
+    }
+    println(mulFinder(fileData))
+}
+fun mulFinder(myList : MutableList<String>): MutableList<String> {
+    val mulList = mutableListOf<String>()
+    for(i in myList.indices){
+        val mulPart = myList[i].substring(0,4)
+        for(j)
+           if(mulPart.equals("mul(")){
+           mulList.add(mulPart)
+        }
+    }
+    return mulList
+}
