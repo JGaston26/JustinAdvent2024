bf8ce10590a60585e99a23658713aa13c2ef4717
JGaston26 <125668861+JGaston26@users.noreply.github.com>
2024-12-19 14:50:56 -0500

    Update Day11.kt

--- Day11/Day11.kt
+++ Day11/Day11.kt
@@ -1,18 +1,17 @@
-package Day11
+
+package Day11.Day11
 
 import java.io.File
 import java.io.InputStream
 
 fun main() {
-    val inputFile : File = File("Day11/Day11_Input")
+    val inputFile : File = File("src/Day11/Day11_Input")
     val inputStream : InputStream = inputFile.inputStream()
     val fileData = mutableListOf<String>()
     inputStream.bufferedReader().forEachLine {
         fileData.add(it)
     }
-    for(i in 0..5){
-        blinkCheck(fileData)
-    }
+    doBlink(fileData,5)
     /*
    val stringData = fileData[0]
     val workingList = mutableListOf<String>()
@@ -33,33 +32,31 @@ fun main() {
 
      */
 }
-fun blinkCheck(myList: MutableList<String>) : MutableList<Int>{
-    var count = 0;
+fun doBlink(stoneList: MutableList<String>, blinkCount: Int) {
+    for(i in 0..blinkCount){
+        blinkCheck(stoneList,i)
+
+    }
+}
+fun blinkCheck(myList: MutableList<String>, count: Int){
     val stringData = myList[0]
     val workingList = mutableListOf<Int>()
     val newList = stringData.split(" ").map { it.trim() }
     for( i in newList.indices){
         workingList.add(newList[i].toInt())
     }
-    val temp = mutableListOf<Int>()
-            val stringForm = workingList[count].toString()
+    val stringForm = workingList[count].toString()
 
-            if (workingList[count] == 0) {
-                workingList[count] = 1
-                println(workingList)
-                count++
-            } else if (stringForm.length % 2 == 0) {
-                workingList.add(count + 1, stringForm.substring(stringForm.length / 2).toInt())
-                workingList.add(count + 1, stringForm.substring(0, stringForm.length / 2).toInt())
-                workingList.removeAt(count)
-                println(workingList)
-                count += 2;
-            } else {
-                workingList[count] = workingList[count] * 2024
-                println(workingList)
-                count++
-            }
-            println(count)
-        println(temp)
-    return temp
+    if (workingList[count] == 0) {
+        workingList[count] = 1
+        println(workingList)
+    } else if (stringForm.length % 2 == 0) {
+        workingList.add(count + 1, stringForm.substring(stringForm.length / 2).toInt())
+        workingList.add(count + 1, stringForm.substring(0, stringForm.length / 2).toInt())
+        workingList.removeAt(count)
+        println(workingList)
+    } else {
+        workingList[count] = workingList[count] * 2024
+        println(workingList)
+    }
 }
